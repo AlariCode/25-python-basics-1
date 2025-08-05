9c8463bcf06daac3a3d13e344a710d4dc20b7975
Alaricode <antonlarichev@gmail.com>
2025-08-05 18:08:52 +0300

    Переменные и значения

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Привет")
+# Правильные названия
+num = 5
+my_num = 1
+_my_num = 1
+myNum = 1
+MYNUM = 1
+num1 = 1
+print(num)
+
+# Неправильные названия
+# 2num = 2
+# my-num = 4
+# my num = 3
