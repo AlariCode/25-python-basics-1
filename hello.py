4c8732c1ce2f70d2887b3b8f03b5116aec6778fe
Alaricode <antonlarichev@gmail.com>
2025-08-06 12:26:24 +0300

    Типы

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 user_age = 18
+salary = 100.5 + 50
 is_admin = True
 username = "Anton"
 
 # a = 1
 # b = 4
+user_age = "18"
 position_x, position_y = 1, 4
-
-print(user_age)
-print(username)
+type_of_user_age = type(user_age)
+print(type_of_user_age)
+print(type(salary))
+print(type(is_admin))
+print(type(username))
