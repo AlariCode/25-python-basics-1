3f3628deba07f9642ab3cd5a3e269d930790ce8a
Alaricode <antonlarichev@gmail.com>
2025-08-15 11:24:00 +0300

    Строки

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
-t = (1, 2, 3)
-t1 = 1, 2, 3
-print(t[0])
-print(t[-1])
+name = "Anton"
+print(type(name))
+print(name[0])
+print(name[-1])
+# print(name[5])
+# name[0] = "U"
+print(len(name))
+t = tuple(name)
+l = list(name)
 
-t = (1, "1", [1, 2])
-t[-1][0] = 8
-print(len(t))
-
-l = list(t)
+print(t)
 print(l)
+
+sep = "=" * 20
+print(sep)
+
+tu = (1, 2) * 20
+print(tu)
