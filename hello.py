bfd59d950f1e8df681cad61a3bc96f1d2825dd1d
Alaricode <antonlarichev@gmail.com>
2025-08-18 19:53:22 +0300

    Slicing

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
-a = [10, 0]
-b = a
-a[0] = 3
+a = [[0, 1], 2, 3, 4, 5]
+print(a[0:3])
+print(a[:3])
+print(a[2:])
+b = a[:]
+a[0][0] = 10
 print(a)
-print(id(a))
 print(b)
-print(id(b))
+
+c = [0, 1, 2, 3, 4, 5, 6]
+print(c[0:5:2])
+print(c[0::2])
+print(c[::3])
+print(c[-4:-1:2])
+print(c[-1:-4:-1])
