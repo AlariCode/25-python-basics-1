ea10713208e41edae22e88a93819c33b0fa95ccf
Alaricode <antonlarichev@gmail.com>
2025-08-20 19:49:49 +0300

    Копирование

--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
-l = [1, 2, 3, 4, 5]
-# l[0:2] = (7, 8, 9, 10)
-# l[:2] = [7, 8]
-# l[:2] = "abc"
-# del l[0]
-# del l[:2]
-l.append(6)
-# l.extend((6, 7))
-# l.extend("abc")
-l.insert(0, 0)
+l = [[1, 2], [2, 3]]
+c = l.copy()
+print(c is l)
+l[0][0] = 10
+print(c)
 print(l)
+print(l[0] is c[0])
