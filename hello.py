c0a958b9c63fc58b416c142cde51ba4fae6061fa
Alaricode <antonlarichev@gmail.com>
2025-08-22 13:28:57 +0300

    Упражнение - Письмо

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
-name, email, age = "Anton", "a@a.ru", 18
-print("Привет " + name + " с почтой " + email + " и " + str(age) + " лет")
-print("Привет {} с почтой {} и {} лет".format(name, email, age))
-print("Привет {n} с почтой {e} и {a} лет".format(e=email, a=age, n=name))
-print(f"Привет {name} с почтой {email} и {age} лет")
-print(f"Привет {name} с почтой {email} и {age + 10} лет")
-print(f"Привет {name} с почтой {email} и {age:.1f} лет")
+# Здравствуйте, Антон!
+# Ваш счёт к оплате: 3499.90 ₽
+# ——————————————————————————————
+# Прошу оплатить сегодня
+
+name, total = "Антон", 3499.9
+
+EMAIL = (
+    f"Здравствуйте, {name}!\n"
+    f"Ваш счёт к оплате: {total:.2f} ₽\n"
+    f"{'-'*30}\n"
+    "Прошу оплатить сегодня"
+)
+
+print(EMAIL)
