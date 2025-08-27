7f0fa59e0098692f5472e6470d9031e53351e9e5
Alaricode <antonlarichev@gmail.com>
2025-08-27 09:28:25 +0300

    Цикл for

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
-print(list(range(10)))
-print(list(range(5, 10)))
-print(type(range(5, 10)))
-print(list(range(5, 11, 2)))
-print(list(range(10, 0, -1)))
+names = ["Anton", "Vasia", "Kate"]
+
+for name in names:
+    print(f"Привет {name}")
+print("Готово")
+
+for num in range(5):
+    print(num)
+print("Готово")
+
+su = [10, 20, 30, 100]
+res = 0
+
+for s in su:
+    res += s
+print(res)
