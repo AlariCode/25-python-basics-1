5decdf801577aa69d1a9a5eff4230f037dc5d02e
Alaricode <antonlarichev@gmail.com>
2025-08-28 09:49:31 +0300

    Break continue else

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# Угадай число
+for i in range(100):
+    print(i)
+    if i >= 10:
+        print("Конец цикла")
+        break
+print("Готово")
 
-secret_number = 7
-guess = 0
+for i in range(1, 11):
+    if i % 2 == 1:
+        continue
+    print(i)
 
-print("Угадай число от 1 до 10")
-i = 0
-while i < 10:
-    print("Сломался")
-    i -= 10
-
-while guess != secret_number:
-    guess = int(input("Введите число: "))
-    if guess < secret_number:
-        print("Загаданное число больше")
-    elif guess > secret_number:
-        print("Загаданное число меньше")
-print("Поздравляю, вы угадали!")
+for i in range(5):
+    print(i)
+    if i > 3:
+        break
+else:
+    print("Готово без break")
