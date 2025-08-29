160fb899ca02f25690fb97a1330827dab4cfa8c3
Alaricode <antonlarichev@gmail.com>
2025-08-29 10:21:08 +0300

    Упражнение - Калькулятор

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-# def my_func():
-#     x = 10
-#     print(x)
+# Написать функцию calculate, которая принимает 2 числа и операцию
+# "+" "-" "*" "/"
+
+def calculate(a: float, b: float, operation: str):
+    match operation:
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            return a / b if b != 0 else "Ошибка: деление на 0"
+        case _:
+            return "Неизвестная операция"
 
 
-# my_func()
-# print(x)
-
-
-def show():
-    print(x)
-
-
-x = 20
-show()
+print(calculate(10, 5, "+"))
+print(calculate(10, 0, "/"))
