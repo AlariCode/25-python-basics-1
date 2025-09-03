f157385437b3914afdebc537b24d895eb391d922
Alaricode <antonlarichev@gmail.com>
2025-09-03 09:45:29 +0300

    Доступ к значениям

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-user = {"age": 37, True: 1}
-empty = {}
-country = dict(germany="Berlin", england="London")
-pairs = dict([("a", 1), ("b", 2)])
+user = {"age": 37, "name": 1}
+print(user["name"])
+user["age"] = 20
+print(user)
+
+# print(user["city"])
+print(user.get("city", "Moscow"))
+value = user.pop("age")
+print(value)
+print(user)
+
+exist = "name" in user
+print(exist)
