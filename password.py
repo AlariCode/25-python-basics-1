2888f18d72c757fccf72573b79aaa0bb33d6c809
Alaricode <antonlarichev@gmail.com>
2025-09-03 09:52:03 +0300

    Упражнение - Меню приложения

--- password.py
+++ password.py
@@ -1,8 +1,3 @@
-# Сделать функцию - Генератор паролей, принимающий
-# length
-# use_symbols
-# понадобится random.choice()
-
 import random
 import string
 
@@ -24,5 +19,25 @@ def generate_password(length: int = 8, use_symbols: bool = True):
     return "".join(password_chars)
 
 
-print(generate_password())
-print(generate_password(10, False))
+def show_menu():
+    print("1. Показать пароли")
+    print("2. Добавить пароль")
+    print("3. Удалить пароль")
+    print("4. Обновить пароль")
+    print("5. Выход")
+    user_slect = int(input("Ваш выбор (1,2,3,4,5): "))
+    match user_slect:
+        case 1:
+            print("Показать пароли")
+        case 2:
+            print("Добавить пароль")
+        case 3:
+            print("Удалить пароль")
+        case 4:
+            print("Обновить пароль")
+        case _:
+            exit()
+
+
+while True:
+    show_menu()
