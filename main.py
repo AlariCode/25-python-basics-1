8f76fc25fa91bcd772bf43741bbe422a75caada1
Alaricode <antonlarichev@gmail.com>
2025-09-05 09:47:07 +0300

    Lambda

--- main.py
+++ main.py
@@ -2,18 +2,5 @@ def apply(func, value):
     return func(value)
 
 
-def square(x: int):
-    return x * x
-
-
-print(apply(square, 5))
-
-
-def make_adder(n: int):
-    def adder(x: int):
-        return x + n
-    return adder
-
-
-add5 = make_adder(5)
-print(add5(10))
+print(apply(lambda x: x + 100, 5))
+print(apply(lambda s: s.upper(), "hi"))
