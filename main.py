75e6f7dab508c61ab738535fbd3d46d7e397223e
Alaricode <antonlarichev@gmail.com>
2025-09-05 08:57:46 +0300

    Упражнение - Проверка документов

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-a = {1, 2}
-b = {1, 2, 3}
-c = {3, 1, 2}
+# 1.	Проверьте, что у пользователя есть все обязательные документы
+# 2.	Равны ли наборы документов?
+# 3.	Выведите список лишних документов, которые пользователь добавил.
 
-print(a.issubset(b))
-print(b.issubset(a))
+required_docs = frozenset({"паспорт", "ИНН", "СНИЛС"})
+user_docs = {"паспорт", "ИНН", "СНИЛС", "диплом"}
 
-print(a.issuperset(b))
-print(b.issuperset(a))
-
-print(c == b)
-print(c != b)
-
-a.clear()
-print(a)
-
-d = b.copy()
-print(d == b)
+print(f"Обязательные: {required_docs.issubset(user_docs)}")
+print(f"Равны ли: {required_docs == frozenset(user_docs)}")
+print(f"Лишние: {user_docs - required_docs}")
