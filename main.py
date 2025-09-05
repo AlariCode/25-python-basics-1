05cb33b8d667b7b417b8ae361c8e66f5319e0b5a
Alaricode <antonlarichev@gmail.com>
2025-09-05 10:38:39 +0300

    filter

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-# Нужно нормализовать данные, убрав пробелы + большая первая буква
-# и привидя age к числу
-users = [
-    {"name": "  anton ", "age": "25"},
-    {"name": "KATE", "age": " 30 "},
-    {"name": "Oleg", "age": "22"}
-]
+num = [10, 15, 20, 25, 30]
+# evens = []
+
+# for n in num:
+#     if n % 2 == 0:
+#         evens.append(n)
+
+# print(evens)
 
 
-def normalize(user: dict[str, str]) -> dict:
-    return {
-        "name": user["name"].strip().capitalize(),
-        "age": int(user["age"].strip())
-    }
+# def is_even(x: int) -> bool:
+#     if x % 2 == 0:
+#         return True
+#     return False
 
 
-normalized_users = list(map(normalize, users))
-print(normalized_users)
+# evens = list(filter(is_even, num))
+# print(evens)
+evens = list(filter(lambda x: x % 2 == 0, num))
+print(evens)
+
+even_comp = [x for x in num if x % 2 == 0]
+print(even_comp)
