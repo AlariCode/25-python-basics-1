ccc4e95d0ff0df546ac831c58fcace686ce6edaf
Alaricode <antonlarichev@gmail.com>
2025-09-05 10:28:08 +0300

    Упражнение - Нормализация данных

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-num = [1, 2, 3, 4, 5]
-# res = []
+# Нужно нормализовать данные, убрав пробелы + большая первая буква
+# и привидя age к числу
+users = [
+    {"name": "  anton ", "age": "25"},
+    {"name": "KATE", "age": " 30 "},
+    {"name": "Oleg", "age": "22"}
+]
 
-# for n in num:
-#     res.append(n * n)
 
-# print(res)
+def normalize(user: dict[str, str]) -> dict:
+    return {
+        "name": user["name"].strip().capitalize(),
+        "age": int(user["age"].strip())
+    }
 
 
-# def square(x: float):
-#     return x * x
-
-# squares_itarator = map(lambda x: x * x, num)
-
-squares = list(map(lambda x: x * x, num))
-print(squares)
-
-# for el in squares_itarator:
-#     print(el)
-
-a = [1, 2, 3]
-b = [10, 20, 30]
-
-sums = list(map(lambda x, y: x + y, a, b))
-print(sums)
+normalized_users = list(map(normalize, users))
+print(normalized_users)
