1bc70dca5d5559d2db8eaffd561aa1fbadca0609
Alaricode <antonlarichev@gmail.com>
2025-09-05 10:16:08 +0300

    map

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def apply(func, value):
-    return func(value)
+num = [1, 2, 3, 4, 5]
+# res = []
+
+# for n in num:
+#     res.append(n * n)
+
+# print(res)
 
 
-print(apply(lambda x: x + 100, 5))
-print(apply(lambda s: s.upper(), "hi"))
+# def square(x: float):
+#     return x * x
+
+# squares_itarator = map(lambda x: x * x, num)
+
+squares = list(map(lambda x: x * x, num))
+print(squares)
+
+# for el in squares_itarator:
+#     print(el)
+
+a = [1, 2, 3]
+b = [10, 20, 30]
+
+sums = list(map(lambda x, y: x + y, a, b))
+print(sums)
