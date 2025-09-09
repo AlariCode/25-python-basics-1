ede8d63d036596c13aea1cb68bfba3060a085a2c
Alaricode <antonlarichev@gmail.com>
2025-09-09 15:22:41 +0300

    Упражнение - Статистика заказов

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+# Список покупок
+# 	1.	Общую сумму всех заказов.
+#   2.	Количество всех проданных товаров.
+
 from functools import reduce
 
 
-nums = [1, 2, 3, 4, 5]
-print(sum(nums))
+orders = [
+    {"id": 1, "user": "Anton", "items": [
+        {"name": "Laptop", "price": 1000},
+        {"name": "Mouse", "price": 50}
+    ]},
+    {"id": 2, "user": "Kate", "items": [
+        {"name": "Phone", "price": 700}
+    ]},
+    {"id": 3, "user": "Oleg", "items": [
+        {"name": "Monitor", "price": 300},
+        {"name": "Keyboard", "price": 100}
+    ]}
+]
+# {"total_price": 1000, "total_items": 10}
 
-res = 0
 
-for n in nums:
-    res += n
-print(res)
+def aggregate(acc: dict, order: dict) -> dict:
+    order_sum = sum(item["price"] for item in order["items"])
+    order_count = len(order["items"])
+    return {
+        "total_price": acc["total_price"] + order_sum,
+        "total_items": acc["total_items"] + order_count
+    }
 
 
-def sum_custom(acc: int, next_el: int):
-    return acc + next_el
-
-
-total = reduce(sum_custom, nums, 10)
-print(total)
-
-words = ["Привет!", "Как", "дела?"]
-sentence = reduce(lambda a, b: a + " " + b, words)
-print(sentence)
+result = reduce(aggregate, orders, {"total_price": 0,  "total_items": 0})
+print(result)
