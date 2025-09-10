823e14c42dd2495c5a4cf0cbaa9b03a6e5a27dda
Alaricode <antonlarichev@gmail.com>
2025-09-10 10:18:32 +0300

    Упражнение - Выплата зарплаты

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-nums = [5, 2, 9, 1, 7, 10]
+# Оставь только name и salary
+# Оставь только активных
+# Их отсортируй по уменьшения зарплаты и выведи
+# Посчитай сумму для выплат
 
-print(sorted(nums))
-print(sorted(nums, reverse=True))
+from functools import reduce
 
-words = ["banana", "apple", "pear", "ab"]
 
-print(sorted(words))
-print(sorted(words, reverse=True))
-
-users = [
-    {"name": "Anton", "age": 18},
-    {"name": "Maria", "age": 20},
-    {"name": "Anna", "age": 20},
-    {"name": "Vasia", "age": 50}
+employees = [
+    {"name": "Anton", "department": "IT", "salary": 120000, "active": True},
+    {"name": "Kate", "department": "HR", "salary": 90000, "active": False},
+    {"name": "Oleg", "department": "IT", "salary": 150000, "active": True},
+    {"name": "Maria", "department": "Finance", "salary": 110000, "active": True},
+    {"name": "Ivan", "department": "IT", "salary": 95000, "active": False},
+    {"name": "Dasha", "department": "Finance", "salary": 130000, "active": True}
 ]
 
-print(sorted(users, key=lambda u: (-u["age"], u["name"])))
+active = filter(lambda e: e["active"], employees)
+mapped = map(lambda e: {"name": e["name"], "salary": e["salary"]}, active)
+sorted_employee = sorted(mapped, key=lambda e: -e["salary"])
+print(sorted_employee)
+total_salary = reduce(lambda acc, e: acc + e["salary"], sorted_employee, 0)
+print(f"Суммарная зарплата: {total_salary}")
