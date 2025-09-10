608d1223afc105170b77ac7b464f1dab94d368aa
Alaricode <antonlarichev@gmail.com>
2025-09-10 10:54:56 +0300

    Замыкание

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-# Оставь только name и salary
-# Оставь только активных
-# Их отсортируй по уменьшения зарплаты и выведи
-# Посчитай сумму для выплат
-
-from functools import reduce
+def multiplier(factor: float):
+    def inner(x: float):
+        return x * factor
+    return inner
 
 
-employees = [
-    {"name": "Anton", "department": "IT", "salary": 120000, "active": True},
-    {"name": "Kate", "department": "HR", "salary": 90000, "active": False},
-    {"name": "Oleg", "department": "IT", "salary": 150000, "active": True},
-    {"name": "Maria", "department": "Finance", "salary": 110000, "active": True},
-    {"name": "Ivan", "department": "IT", "salary": 95000, "active": False},
-    {"name": "Dasha", "department": "Finance", "salary": 130000, "active": True}
-]
+double = multiplier(2)
+triple = multiplier(3)
 
-active = filter(lambda e: e["active"], employees)
-mapped = map(lambda e: {"name": e["name"], "salary": e["salary"]}, active)
-sorted_employee = sorted(mapped, key=lambda e: -e["salary"])
-print(sorted_employee)
-total_salary = reduce(lambda acc, e: acc + e["salary"], sorted_employee, 0)
-print(f"Суммарная зарплата: {total_salary}")
+print(double(10))
+print(triple(10))
+
+
+def make_counter():
+    count = 0
+
+    def counter():
+        nonlocal count
+        count += 1
+        return count
+    return counter
+
+
+c1 = make_counter()
+c2 = make_counter()
+print(c1())
+print(c1())
+print(c1())
+print(c2())
