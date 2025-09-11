fd8b9448719c8f0f6e81b7bd5af13cc82a71da46
Alaricode <antonlarichev@gmail.com>
2025-09-11 13:08:43 +0300

    Упражнение - KeyboardInterrupt

--- main.py
+++ main.py
@@ -2,24 +2,30 @@
 def main():
     print("Task менеджер. help - для справки")
     while True:
-        raw = input("> ").strip()
-        parts = raw.split()
-        cmd, args = parts[0], parts[1:]
-        match cmd:
-            case "help":
-                pass
-            case "add":
-                pass
-            case "remove":
-                pass
-            case "edit":
-                pass
-            case "tags":
-                pass
-            case "exit":
-                break
-            case _:
-                print("Неизвестная команда")
+        try:
+            raw = input("> ").strip()
+            parts = raw.split()
+            cmd, args = parts[0], parts[1:]
+            match cmd:
+                case "help":
+                    pass
+                case "add":
+                    pass
+                case "remove":
+                    pass
+                case "edit":
+                    pass
+                case "tags":
+                    pass
+                case "exit":
+                    break
+                case _:
+                    print("Неизвестная команда")
+        except KeyboardInterrupt:
+            print("\nЗавершение приложения...")
+            break
+        except Exception as e:
+            print(f"[ERROR]: {e}")
 
 
 main()
