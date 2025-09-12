eaa565000caadd10fc8ff0aaa103a12fca2d672b
Alaricode <antonlarichev@gmail.com>
2025-09-12 17:45:12 +0300

    Приватные методы

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-# import calc
 # import calc as c
-from utils.calc import add
+# from utils.calc import add, _secret
+from utils import calc
 
-print(add(1, 5))
+print(calc.add(1, 5))
+print(calc._secret())
