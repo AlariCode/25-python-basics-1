c0b142a7272619b48d878b2c5b6918351a24f0ee
Alaricode <antonlarichev@gmail.com>
2025-09-17 18:28:06 +0300

    Упражнение - Создание задачи

--- commands/tasks.py
+++ commands/tasks.py
@@ -1,17 +1,25 @@
 
 from typing import TypedDict
 
+PRIORITIES = {"low", "med", "high"}
+
 
 class Task(TypedDict):
     id: int
     title: str
     priority: str
+    tags: list[str]
     status: str
 
 
-t: Task = {
-    "id": 1,
-    "title": "Вытерить пыль",
-    "priority": "low",
-    "status": "new"
-}
+def make_task(id_: int, title: str, priority: str = "med", tags: list[str] = []) -> Task:
+    if priority not in PRIORITIES:
+        raise ValueError("Неверный приоритет. Можно только low | med | high")
+    task: Task = {
+        "id": id_,
+        "title": title.strip(),
+        "priority": priority,
+        "tags": tags,
+        "status": "new"
+    }
+    return task
