73ec08dc43027dd7afc81af82932902e393806d9
Alaricode <antonlarichev@gmail.com>
2025-09-17 18:40:10 +0300

    Создание date и time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from commands.help import help_command
+import datetime
 
 
 def main():
@@ -31,4 +32,18 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    now = datetime.datetime.now()
+    print(now)
+    today = datetime.date.today()
+    print(today)
+    current_time = now.time()
+    print(current_time)
+
+    d = datetime.date(2025, 9, 17)
+    print(d.weekday())
+    print(d)
+    t = datetime.time(19, 0, 45)
+    print(t)
+    dt = datetime.datetime(2025, 9, 17, 19, 0, 45)
+    print(dt)
